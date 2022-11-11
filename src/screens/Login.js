cd86897f45440dcaad1aad0eedffb6890c8b280c
Bilal Kucuk <bilalkucuk491@gmail.com>
2022-11-12 00:47:09 +0300

    edited login screen

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -7,23 +7,37 @@ import {
   StatusBar,
 } from 'react-native';
 import {COLORS, FONTS, SIZES, images} from '../constants';
-import {LinearGradient} from "react-native-linear-gradient"
+import LinearGradient from 'react-native-linear-gradient';
 
 const Login = ({navigation}) => {
-
-  const renderHeader = ()=>{
+  const renderHeader = () => {
     return (
-      <View style={{
-        height:SIZES.height > 700 ? "65%":"60%"
-      }}>
-        <ImageBackground source={images.loginBackground} 
+      <View
         style={{
-          flex:1,
-          justifyContent:"flex-end"
-        }} resizeMode="cover" />
+          height: SIZES.height > 700 ? '65%' : '60%',
+        }}>
+        <ImageBackground
+          source={images.loginBackground}
+          style={{
+            flex: 1,
+            justifyContent: 'flex-end',
+          }}
+          resizeMode="cover">
+
+          <LinearGradient
+            start={{x: 0, y: 0}}
+            end={{x: 0, y: 1}}
+            colors={[COLORS.transparent, COLORS.black]}
+            style={{
+              height: 200,
+              justifyContent: 'flex-end',
+              paddingHorizontal: SIZES.padding,
+            }}></LinearGradient>
+
+        </ImageBackground>
       </View>
-    )
-  }
+    );
+  };
 
   return (
     <View
@@ -31,8 +45,8 @@ const Login = ({navigation}) => {
         flex: 1,
         backgroundColor: COLORS.black,
       }}>
-        <StatusBar barStyle="light-content" />
-        {renderHeader()}
+      <StatusBar barStyle="light-content" />
+      {renderHeader()}
       {/* header */}
       {/* Details */}
     </View>
